3d2868b4bf42d50efa48bb6ffbc7f8221632e5c7
orlandev <dfashion.corp@gmail.com>
2022-01-31 16:59:28 -0600

    agregando interfaz de usuario

--- app/src/main/java/com/orlandev/noticias/MainActivity.kt
+++ app/src/main/java/com/orlandev/noticias/MainActivity.kt
@@ -3,14 +3,23 @@ package com.orlandev.noticias
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.orlandev.noticias.ui.theme.NoticiasTheme
 import com.orlandev.noticias.viewModel.NoticiasViewModel
@@ -37,13 +46,48 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NoticiasScreen(noticiasViewModel: NoticiasViewModel = hiltViewModel()) {
-   val stateData= noticiasViewModel.data.collectAsState(null)
+    val stateData = noticiasViewModel.data.collectAsState(null)
 
+    stateData.value?.let { news ->
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(news.hits) { newsInfo ->
+
+                NoticiasItem(newsInfo.title ?: "", newsInfo.created_at, newsInfo.author)
+            }
+        }
+    }
 }
+
+@Composable
+fun NoticiasItem(title: String = "titulo", fecha: String = "fecha", autor: String = "autor") {
+    Card(
+        elevation = 8.dp,
+        shape = RoundedCornerShape(20),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(4.dp)) {
+            Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            Text(text = fecha)
+            Text(text = autor)
+
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NoticiasTheme {
-        NoticiasScreen( )
+        NoticiasScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoticiasItemPreview() {
+    NoticiasTheme {
+        NoticiasItem()
     }
 }
